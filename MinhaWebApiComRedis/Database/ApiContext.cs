bcb30730024fab2b8456e6a1a95a9c17ab50d631
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2021-01-20 19:29:10 -0300

    criado app inicial previsao do tempo com dbcontext em memória

---
+++ MinhaWebApiComRedis/Database/ApiContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaWebApiComRedis.Database
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+        public DbSet<WeatherForecast> PrevisoesClimaticas { get; set; }
+    }
+}
---
+++ MinhaWebApiComRedis/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using MinhaWebApiComRedis.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MinhaWebApiComRedis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApiContext>(x => x.UseInMemoryDatabase("PrevisoesDoTempoDb"));
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
